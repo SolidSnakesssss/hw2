d8503247f53415f03933ea300f22a6277f52a538
Charles Meeler <cmeeler@usc.edu>
2024-02-04 01:01:09 +0000

    Finished Tast 2

--- util.h
+++ util.h
@@ -13,20 +13,41 @@
 template <typename T>
 std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
 {
+    //Create new set
+    std::set<T> newSet;
 
+    //Iterates through set s1
+    for (typename std::set<T>::iterator it = s1.begin(); it != s1.end(); ++it)
+    {
+        //Adds key to newSet if both s1 and s2 have that key
+        if (s2.find(*it) != s2.end())
+        {
+            newSet.insert(*it);
+        }
+    }
 
-
-
-
+    //returns the new set
+    return newSet;
 }
+
 template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
 {
+    //Create new set
+    std::set<T> newSet = s2;
 
+    //Iterates through set s1
+    for (typename std::set<T>::iterator it = s1.begin(); it != s1.end(); ++it)
+    {
+        //Adds the key to newSet if it is not in set s2
+        if (s2.find(*it) == s2.end())
+        {
+            newSet.insert(*it);
+        }
+    }
 
-
-
-
+    //returns the new set
+    return newSet;
 }
 
 /***********************************************/
@@ -39,11 +60,11 @@ std::set<std::string> parseStringToWords(std::string line);
 
 // Used from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
 // Removes any leading whitespace
-std::string &ltrim(std::string &s) ;
+std::string& ltrim(std::string& s);
 
 // Removes any trailing whitespace
-std::string &rtrim(std::string &s) ;
+std::string& rtrim(std::string& s);
 
 // Removes leading and trailing whitespace
-std::string &trim(std::string &s) ;
+std::string& trim(std::string& s);
 #endif
