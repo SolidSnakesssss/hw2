06d49cf27035cc919a9760e52c3b81e9e8bc3739
Charles Meeler <cmeeler@usc.edu>
2024-02-06 17:44:12 +0000

    Maybe task 5 is finsihed???

---
+++ mydatastore.cpp
@@ -0,0 +1,216 @@
+#include "mydatastore.h"
+
+using namespace std;
+
+void MyDataStore::addProduct(Product* p)
+{
+    addProductToMap(p);
+    products_.insert(p);
+    //products_.push_back(p);
+}
+
+void MyDataStore::addUser(User* u)
+{
+    userSet_.insert(convToLower(u->getName()));
+    userMap[convToLower(u->getName())] = u;
+}
+
+void MyDataStore::addProductToMap(Product* p)
+{
+    //Retrieves the keywords of the product
+    set<string> keyWords = p->keywords();
+
+    //Maps the products to the appropriate keys(key terms)
+    for(set<string>::iterator it = keyWords.begin(); it != keyWords.end(); ++it)
+    {
+        productMap[*it].insert(p);
+    }
+}
+
+/**
+     * Performs a search of products whose keywords match the given "terms"
+     *  type 0 = AND search (intersection of results for each term) while
+     *  type 1 = OR search (union of results for each term)
+*/
+std::vector<Product*> MyDataStore::search(std::vector<std::string>& terms, int type)
+{
+    vector<Product*> results;
+
+    if (type == 0)
+    {
+        results = intersectionReturn(terms);
+        return results;
+    }
+
+    else if(type == 1)
+    {
+        results = unionReturn(terms);
+        return results;
+    }
+
+    else return results;
+}
+
+//Finds the intersection of the search results
+std::vector<Product*> MyDataStore::intersectionReturn(std::vector<std::string>& terms)
+{
+    vector<Product*> productReturn;
+    set<Product*> totalProducts;
+
+    //Checks if the map has any keys with the same term
+    for(vector<string>::iterator it = terms.begin(); it != terms.end(); ++it)
+    {
+        //Checks if the term is in the product map
+        map<std::string, set<Product*>>::iterator it1 = productMap.find(*it);
+
+        //Executes when the term is found
+        if (it1 != productMap.end())
+        {
+            //Creates a new set to equal the set of productMap(*it)
+            std::set<Product*>& productSet = productMap[*it];
+            set<Product*> tempProductsSet;
+
+            //Adds products to the tempProductSet
+            for (Product* product : productSet)
+            {
+                tempProductsSet.insert(product);
+            }
+
+            //Gets wthe intersection of the current totalProducts and tempProductSet returning a new set
+            //with only common products
+            totalProducts = setIntersection(totalProducts, tempProductsSet);
+        }
+    }
+
+    for(set<Product*>::iterator it = totalProducts.begin(); it != totalProducts.end(); ++it)
+    {
+        productReturn.push_back(*it);
+    }
+
+    //returns the product vector
+    return productReturn;
+}
+
+//Finds the union of the searc results
+std::vector<Product*> MyDataStore::unionReturn(std::vector<std::string>& terms)
+{
+    vector<Product*> productReturn;
+    set<Product*> totalProducts;
+
+    //Checks if the map has any keys with the same term
+    for(vector<string>::iterator it = terms.begin(); it != terms.end(); ++it)
+    {
+        //Checks if the term is in the product map
+        map<std::string, set<Product*>>::iterator it1 = productMap.find(*it);
+
+        //Executes when the term is found
+        if (it1 != productMap.end())
+        {
+            //Creates a new set to equal the set of productMap(*it)
+            std::set<Product*>& productSet = productMap[*it];
+            set<Product*> tempProductsSet;
+
+            //Adds products to the tempProductSet
+            for (Product* product : productSet)
+            {
+                tempProductsSet.insert(product);
+            }
+
+            //Gets wthe Union of the current totalProducts and tempProductSet returning a new set
+            //with only common products
+            totalProducts = setUnion(totalProducts, tempProductsSet);
+        }
+    }
+
+    for(set<Product*>::iterator it = totalProducts.begin(); it != totalProducts.end(); ++it)
+    {
+        productReturn.push_back(*it);
+    }
+
+    //returns the product vector
+    return productReturn;
+}
+
+void MyDataStore::addToCart(std::string username, Product* index)
+{
+    string usernameLower = convToLower(username);
+    set<string>::iterator validUser  = userSet_.find(usernameLower);
+
+    if(validUser == userSet_.end())
+    {
+        cout << "User not found!" << endl;
+        return;
+    }
+    
+    cart[username].push_back(index);
+}
+
+void MyDataStore::displayCart(std::string username)
+{
+    string usernameLower = convToLower(username);
+    int count = 0;
+
+    set<string>::iterator validUser  = userSet_.find(usernameLower);
+
+    if(validUser == userSet_.end())
+    {
+        cout << "User not found!" << endl;
+        return;
+    }
+
+    for(vector<Product*>::iterator it = cart[usernameLower].begin(); it != cart[usernameLower].end(); ++it)
+    {
+        Product* temp = *it;
+        cout <<  "Item #" << ++count << ": " << temp->getName() << endl;
+    }
+}
+
+
+//Buys Items from the user's cart (Do not need to implement item removal)
+void MyDataStore::buyUserCart(std::string username)
+{
+    string usernameLower = convToLower(username);
+
+    set<string>::iterator validUser  = userSet_.find(usernameLower);
+
+    if(validUser == userSet_.end())
+    {
+        cout << "User not found!" << endl;
+        return;
+    }
+
+    User* tempUser = userMap[usernameLower];
+
+    for(vector<Product*>::iterator it = cart[usernameLower].begin(); it != cart[usernameLower].end(); ++it)
+    {
+        Product* tempProduct = *it;
+
+        if(tempProduct->getQty() > 0)
+        {
+            if(tempUser->getBalance() >= tempProduct->getPrice())
+            {
+                tempUser->deductAmount(tempProduct->getPrice());
+                tempProduct->subtractQty(1);
+            }
+
+            else continue;
+        }
+
+        else continue;
+    }
+}
+
+void MyDataStore::dump(std::ostream& ofile)
+{
+    for(set<Product*>::iterator it = products_.begin(); it != products_.end(); ++it)
+    {
+        Product* temp = *it;
+        temp->dump(ofile);
+    }
+
+    for(set<User*>::iterator it = users_.begin(); it != users_.end(); ++it)
+    {
+        User* temp = *it;
+        temp->dump(ofile);
+    }
+}
--- mydatastore.h
+++ mydatastore.h
@@ -2,5 +2,46 @@
 #define MYDATASTORE_H
 
 #include "datastore.h"
+#include "util.h"
+#include "book.h"
+#include "clothing.h"
+#include "movie.h"
+#include "product.h"
+#include <map>
+#include <set>
+
+class MyDataStore : public DataStore
+{
+public:
+    ~MyDataStore(){}
+
+    void addProduct(Product* p);
+
+    void addUser(User* u);
+
+    std::vector<Product*> search(std::vector<std::string>& terms, int type);
+
+    void dump(std::ostream& ofile);
+
+    void addProductToMap(Product* p);
+
+    void addToCart(std::string username, Product* index);
+
+    void displayCart(std::string username);
+
+    void buyUserCart(std::string username);
+
+    std::vector<Product*> intersectionReturn(std::vector<std::string>& terms);
+    std::vector<Product*> unionReturn(std::vector<std::string>& terms);
+
+
+private:
+    std::set<Product*> products_;
+    std::set<User*> users_;
+    std::set<std::string> userSet_;
+    std::map<std::string, std::set<Product*>> productMap;
+    std::map<std::string, std::vector<Product*>> cart;
+    std::map<std::string, User*> userMap;
+};
 
 #endif
